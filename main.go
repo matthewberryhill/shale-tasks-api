c26e3f45984df0af263a503517cf8056f60113a9
Matthew Berryhill <matthewberryhill@gmail.com>
2018-10-28 16:25:24 -0600

    https://github.com/matthewberryhill/shale-db/issues/1 Implement environment flags to connect to mongo replicaset

--- main.go
+++ main.go
@@ -1,13 +1,42 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/matthewberryhill/shale-tasks-api/config"
 	"github.com/matthewberryhill/shale-tasks-api/server"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/tkanos/gonfig"
+)
+
+var env = flag.String(
+	"env",
+	"dev",
+	"config env",
 )
 
 func main() {
+	flag.Parse()
+	var configPath string
+
+	env := os.Getenv("MONGO")
+	if env == "dev" {
+		configPath = "./config/dev.json"
+	} else {
+		configPath = "./config/prod.json"
+	}
+	conf := config.Config{}
+	err := gonfig.GetConf(configPath, &conf)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(conf)
+
 	e := echo.New()
 
 	e.Use(middleware.Logger())
